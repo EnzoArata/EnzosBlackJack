14bf983a941144a828c2c67258c85e3ce0420bf5
EnzoArata\laptop <enzoarata50@gmail.com>
2019-04-03 10:27:49 -0700

    Menu Changes and additions
    -Added description for twist
    -Added back button from description

--- Assets/mainMenuManager.cs
+++ Assets/mainMenuManager.cs
@@ -8,15 +8,21 @@ public class mainMenuManager : MonoBehaviour
 {
     public Button playButton;
     public Button rulesButton;
+    public Button homeButton;
     public GameObject playGameObj;
     public GameObject rulesGameObj;
+    public GameObject homeGameObj;
+    public GameObject titleObj;
+    public GameObject descriptionObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        playGameObj.SetActive(true);
-        rulesGameObj.SetActive(true);
+        homeGameObj.SetActive(false);
+        descriptionObj.SetActive(false);
         playButton.onClick.AddListener(selectPlay);
         rulesButton.onClick.AddListener(selectRules);
+        homeButton.onClick.AddListener(selectHome);
     }
 
     // Update is called once per frame
@@ -32,6 +38,19 @@ public class mainMenuManager : MonoBehaviour
 
     public void selectRules()
     {
+        playGameObj.SetActive(false);
+        rulesGameObj.SetActive(false);
+        homeGameObj.SetActive(true);
+        titleObj.SetActive(false);
+        descriptionObj.SetActive(true);
+    }
 
+    public void selectHome()
+    {
+        playGameObj.SetActive(true);
+        rulesGameObj.SetActive(true);
+        homeGameObj.SetActive(false);
+        titleObj.SetActive(true);
+        descriptionObj.SetActive(false);
     }
 }
